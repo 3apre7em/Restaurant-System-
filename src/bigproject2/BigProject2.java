e7f46c1d9676e201fcd47b245c7ab9eb65f13bd2
mostafa abdelraheem <mostafaabdelraheem56@gmail.com>
2023-02-14 12:58:17 +0200

    upload project files

---
+++ src/bigproject2/BigProject2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bigproject2;
+
+/**
+ *
+ * @author Abdelrheem
+ */
+public class BigProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ResturantWindow r1 = new ResturantWindow();
+       r1.setVisible(true);
+    }
+    
+}
